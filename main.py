28f55c36fc7ff95366e47570d240649ba98269d1
Ito Hayato <131941304+hayato19@users.noreply.github.com>
2025-11-15 19:05:28 +0900

    -implement for euler step  -improve argument redundancy

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def set_particle_params(M, posit):
 
     return m_arr, k_arr, kl_arr, gamma_arr, S0_arr, delta_arr
 
-# --- オイラー法（放射力 log 追加） ---
+# --- オイラー法 ---
 def euler_step_multi(m, k, x, v, dt, N, alpha, eps, S0, kl, gamma, delta):
 
     M = x.shape[1]
@@ -89,7 +89,7 @@ def euler_step_multi(m, k, x, v, dt, N, alpha, eps, S0, kl, gamma, delta):
                 ai += -(alpha / m[i]) * dx / (r3 + eps**3)
 
             a[i] += ai
-            f = cooling_step(n, v, i, S0, kl, gamma, delta, hbar_f)
+            f = cooling_step(v[n,i], S0[i], kl[i], gamma[i], delta[i], hbar_f)
             a[i] += f / m[i]
 
         v[n+1, :] = v[n, :] + dt * a[:]
@@ -98,41 +98,37 @@ def euler_step_multi(m, k, x, v, dt, N, alpha, eps, S0, kl, gamma, delta):
     return x, v
 
 # --- 4次ルンゲクッタ法 ---
-def rk4_step_multi(m, k, x, v, dt, N):
+def rk4_step_multi(m, k, x, v, dt, N, S0, kl, gamma, delta):
 
     hbar_f = float(hbar)
-    def f(xn, vn):
+    def f(xn, vn, S0, kl, gamma, delta):
 
         dxdt = vn
-        # ---- 相互作用（全対全, 自己相互作用は除外）----
         # dx[i, j] = x_j - x_i
         dx = xn[np.newaxis, :] - xn[:, np.newaxis]   # (M, M)
         np.fill_diagonal(dx, 0.0)                    # 自己項を0に
         r3 = abs(dx ** 3)                            # 相対変位３乗
         a_int = -(alpha/m) * np.sum(dx / (r3 + eps ** 3), axis=1)
+        f = cooling_step(dxdt,S0[:],kl[:],gamma[:],delta[:], hbar_f)
         dvdt = -(k/m) * xn + a_int
         return dxdt, dvdt
 
     for n in range(N):
         xn, vn = x[n, :], v[n, :]
 
-        k1x, k1v = f(xn, vn)
-        k2x, k2v = f(xn + 0.5*dt*k1x, vn + 0.5*dt*k1v)
-        k3x, k3v = f(xn + 0.5*dt*k2x, vn + 0.5*dt*k2v)
-        k4x, k4v = f(xn + dt*k3x,     vn + dt*k3v)
+        k1x, k1v = f(xn, vn, S0, kl, gamma, delta)
+        k2x, k2v = f(xn + 0.5*dt*k1x, vn + 0.5*dt*k1v, S0, kl, gamma, delta)
+        k3x, k3v = f(xn + 0.5*dt*k2x, vn + 0.5*dt*k2v, S0, kl, gamma, delta)
+        k4x, k4v = f(xn + dt*k3x,     vn + dt*k3v, S0, kl, gamma, delta)
 
         x[n+1, :] = xn + (dt/6.0) * (k1x + 2*k2x + 2*k3x + k4x)
         v[n+1, :] = vn + (dt/6.0) * (k1v + 2*k2v + 2*k3v + k4v)
 
     return x, v
 
-def cooling_step(n, v, i, S0, kl, gamma, delta, h):
-    if gamma[i] != 0.0 and kl[i] != 0.0 and S0[i] != 0.0:
-        denom = (S0[i] + 1.0 + 4.0 / gamma[i] ** 2 * (delta[i] - kl[i] * v[n, i]) ** 2)
-        f = h * gamma[i] * S0[i] / 2.0 / denom * kl[i]
-    else:
-        f = 0
-
+def cooling_step(v, S0, kl, gamma, delta, h):
+    denom = (S0 + 1.0 + 4.0 / gamma ** 2 * (delta - kl * v) ** 2)
+    f = h * gamma * S0 / 2.0 / denom * kl
     return f
 
 # --- 実行 ---
@@ -152,6 +148,7 @@ t, xM, vM = initialize_arrays_multi(M, N, dt, x0s, v0s)
 
 xM, vM = rk4_step_multi(
     m_arr, k_arr, xM, vM, dt, N,
+    S0=S0_arr, kl=kl_arr, gamma=gamma_arr, delta=delta_arr
 )
 print(f"t_final = {t[-1]:.3f}  |  x_last (per particle) = {xM[-1, :]}")
 
